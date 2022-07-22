e9761df454de7cf9104202381e65effba7ac2e18
paulamachaddo <104164061+paulamachaddo@users.noreply.github.com>
2022-07-22 18:11:47 -0300

    First commit

---
+++ chess-system/src/boardgame/Board.java
@@ -0,0 +1,75 @@
+package boardgame;
+
+public class Board {
+	
+	private int rows;
+	private int columns;
+	private Piece[][] pieces;
+	
+	public Board(int row, int column) {
+		if (row < 1 || column < 1) {
+			throw new BoardException("Error creating board: there must be at least 1 row and 1 column");
+		}
+		this.rows = row;
+		this.columns = column;
+		pieces = new Piece[row][column];
+	}
+	
+	public int getRows() {
+		return rows;
+	}
+	
+	public int getColumns() {
+		return columns;
+	}
+	
+	public Piece piece(int row, int column) {
+		if (!positionExists(row, column)) {
+			throw new BoardException("Position not one the board");
+		}
+		return pieces[row][column];
+	}
+	
+	public Piece piece(Position position) {
+		if (!positionExists(position)) {
+			throw new BoardException("Position not one the board");
+		}
+		return pieces[position.getRow()][position.getColumn()];
+	}
+	
+	public void placePiece(Piece piece, Position position) {
+		if (thereIsAPiece(position)) {
+			throw new BoardException("There is already a peace on position: " + position);
+		}
+		pieces[position.getRow()][position.getColumn()] = piece;
+		piece.position = position;
+	}
+	
+	public Piece removePiece (Position position) {
+		if (!positionExists(position)) {
+			throw new BoardException("Position not on the board");
+		}
+		if (piece(position) == null) {
+			return null;
+		}
+		Piece aux = piece(position);
+		aux.position = null;
+		pieces[position.getRow()][position.getColumn()] = null;
+		return aux;
+	}
+
+	public boolean positionExists(int row, int column) {
+		return row >= 0 && row < rows && column >= 0 && column < columns;
+	}
+
+	public boolean positionExists(Position position) {
+		return positionExists(position.getRow(), position.getColumn());
+	}
+	
+	public boolean thereIsAPiece (Position position) {
+		if (!positionExists(position)) {
+			throw new BoardException("Position not one the board");
+		}
+		return piece(position) != null;
+	}
+}
---
+++ chess-system/src/chess/ChessException.java
@@ -0,0 +1,11 @@
+package chess;
+
+import boardgame.BoardException;
+
+public class ChessException extends BoardException{
+	private static final long serialVersionUID = 6492993152489147354L;
+	
+	public ChessException(String msg) {
+		super(msg);
+	}
+}
